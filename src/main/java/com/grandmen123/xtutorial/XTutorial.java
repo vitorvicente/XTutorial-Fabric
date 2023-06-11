9cf790e10ae79a6e06d73bf64b4b06f14ad791df
Victor Vicente <vitor@bu.edu>
2023-06-11 15:17:56 +0100

    Initial Commit

---
+++ src/main/java/com/grandmen123/xtutorial/XTutorial.java
@@ -0,0 +1,15 @@
+package com.grandmen123.xtutorial;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class XTutorial implements ModInitializer {
+    public static final String MOD_ID = "xtutorial"
+;    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hello Fabric world!");
+    }
+}
---
+++ src/main/java/com/grandmen123/xtutorial/XTutorialClient.java
@@ -0,0 +1,10 @@
+package com.grandmen123.xtutorial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class XTutorialClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
