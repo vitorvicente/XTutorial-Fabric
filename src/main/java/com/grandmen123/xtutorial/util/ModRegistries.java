80bb22afe8f071d88c919f6a63357f55c266a46a
Victor Vicente <vitor@bu.edu>
2023-07-22 17:40:25 +0100

    Version 0.2.4 - Adding Composter Fuel

--- src/main/java/com/grandmen123/xtutorial/util/ModRegistries.java
+++ src/main/java/com/grandmen123/xtutorial/util/ModRegistries.java
@@ -2,15 +2,22 @@ package com.grandmen123.xtutorial.util;
 
 import com.grandmen123.xtutorial.item.ModItems;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.block.ComposterBlock;
 
 public class ModRegistries {
     public static void registerModStuffs() {
         registerFuels();
+        registerModCompostables();
     }
 
     private static void registerFuels() {
         FuelRegistry registry = FuelRegistry.INSTANCE;
 
         registry.add(ModItems.PEAT_BRICK, 200);
+    }
+
+    private static void registerModCompostables() {
+        ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(ModItems.CAULIFLOWER, 0.65f);
+        ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(ModItems.CAULIFLOWER_SEEDS, 0.3f);
     }
 }
