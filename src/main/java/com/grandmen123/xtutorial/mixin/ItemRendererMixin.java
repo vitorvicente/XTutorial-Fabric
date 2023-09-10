44ddae318be84c74afb87fca1e214f555e25d3b7
Victor Vicente <vitor@bu.edu>
2023-09-10 16:51:40 -0700

    Version 0.2.26 - Animated Items

--- src/main/java/com/grandmen123/xtutorial/mixin/ItemRendererMixin.java
+++ src/main/java/com/grandmen123/xtutorial/mixin/ItemRendererMixin.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(ItemRenderer.class)
 public abstract class ItemRendererMixin {
 
-    @ModifyVariable(method = "renderItem*", at = @At(value = "HEAD"), argsOnly = true)
+    @ModifyVariable(method = "renderItem", at = @At(value = "HEAD"), argsOnly = true)
     public BakedModel useRadiationStaffModel(BakedModel value, ItemStack stack, ModelTransformationMode renderMode,
                                              boolean leftHanded, MatrixStack matrixStack,
                                              VertexConsumerProvider consumer, int light, int overlay) {
